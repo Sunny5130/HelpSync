9c7a5b8c284c2782448935bd9fa0f883ae73883e
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-09-10 00:52:48 +0530

    Backend update

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ import { app, server } from "./socket.js";
 connectDB();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://help-sync.vercel.app"],
     credentials: true,
   })
 );
